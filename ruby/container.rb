41714d54afe2db4138446773a3623789610fcc36
kimhyunjin <khjzzm@gmail.com>
2017-09-18 21:19:07 +0900

    container array

---
+++ ruby/container.rb
@@ -0,0 +1,21 @@
+# Array
+names = ['khjzzm', 'pcer', 'kimuzzang']
+puts(names.class)
+print(names)
+print("\n")
+
+kimhyunjin = ['programmer', 'seoul', 27, true, ['red','blue']]
+print(kimhyunjin)
+print("\n")
+print(kimhyunjin[4][0])
+print("\n")
+
+
+
+al = ['A', 'B', 'C', 'D']
+puts(al.length)
+al.push('E')
+print(al)
+print("\n")
+al.delete_at(0)
+print(al)
