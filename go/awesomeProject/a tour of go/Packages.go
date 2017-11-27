4cde3495d5b1bed3e7c70555203db26933548935
kimhyunjin <khjzzm@gmail.com>
2017-11-28 00:25:41 +0900

    https://go-tour-kr.appspot.com/#15

---
+++ go/awesomeProject/a tour of go/Packages.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//import "fmt"
+//import "math"
+
+func main() {
+	fmt.Print("Happy", math.Pi, "Day")
+}
---
+++ go/awesomeProject/a tour of go/Variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var x,y,z int = 1, 2, 3
+var c, python, java string = "c", "python", "java"
+var yes, no, blah = true, false, "text"
+
+func main() {
+	fmt.Println(x, y, z, yes, no, blah)
+	fmt.Println(c, python, java)
+
+}
