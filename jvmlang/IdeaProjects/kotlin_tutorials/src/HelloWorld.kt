28a86b9a07189f0a7ca655e85d9b92802882b899
kimhyunjin <khjzzm@gmail.com>
2017-11-29 22:44:37 +0900

    kotlin tutorials

--- jvmlang/IdeaProjects/kotlin_tutorials/src/HelloWorld.kt
+++ jvmlang/IdeaProjects/kotlin_tutorials/src/HelloWorld.kt
@@ -1,3 +1,9 @@
 fun main(args: Array<String>) {
     println("Hello World")
+
+    val a = 1
+    val b: Int = 5
+
+    println(a)
+    print(b)
 }
---
+++ jvmlang/IdeaProjects/kotlin_tutorials/src/TypeCasting.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var num = 3
+    var shortNum: Short = num.toShort()
+    var longNum: Long = num.toLong()
+    var strNum: String = num.toString()
+
+    var str = "10"
+    var num2 = str.toInt()
+
+
+}
