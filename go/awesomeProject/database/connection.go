c2d8a76cfb11090998285511bcd73924152799c5
kimhyunjin <khjzzm@gmail.com>
2017-12-02 22:19:38 +0900

    db connection

---
+++ go/awesomeProject/database/connection.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"io/ioutil"
+)
+
+var (
+	driverName     string = "mysql"
+)
+
+func main() {
+	db, err := sql.Open(driverName, dataSourceName("dbconection.txt"))
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+	defer db.Close()
+
+	var name string
+	err = db.QueryRow("SELECT USER_NAME FROM USER_INFO WHERE USER_ID = 'kimhyunjin'").Scan(&name)
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+	fmt.Println(name)
+
+}
+
+func dataSourceName(filename string) string  {
+	bytes, err := ioutil.ReadFile(filename)
+	if(err != nil) {
+		panic(err)
+	}
+	return string(bytes)
+}
