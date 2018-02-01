2c5e1adfc9f49f025c17e17acb2e68fe6ebfee27
kimhyunjin <khjzzm@gmail.com>
2018-02-01 10:30:03 +0900

    php

---
+++ php/PhpstormProjects/php_test/ConstantsEcho.php
@@ -0,0 +1,16 @@
+<? echo " __FILE__ : " . __FILE__; // 파일의 전체경로와 파일명, 절대경로 ?><br/>
+<? echo " __LINE__ : " . __LINE__; // 파일의 현재 줄 라인  ?><br/>
+<? echo " __DIR__ : " . __DIR__; // dirname함수와 __FILE__상수는 동일한 기능을 하나 마지막에 슬레쉬가 없음 (PHP 5.3에 추가) ?><br/>
+<? echo " __FUNCTION__ : " . __FUNCTION__; // 함수명을 표시 (PHP 5에 추가) ?><br/>
+<? echo " __CLASS__ : " . __CLASS__; // 클레스명을 표시 (PHP 5에 추가) ?><br/>
+<? echo " __METHOD__ : " . __METHOD__; // 클레스 메소드명을 표시 (PHP 5에 추가) ?><br/>
+<? echo " __NAMESPACE__ : " . __NAMESPACE__; // 이 상수는 컴파일 시에 정의됩니다. (PHP 5.3에 추가) ?><br/>
+<? echo " PHP_VERSION : " . PHP_VERSION; // 서버에 설치된 PHP버전 ?><br/>
+<? echo " PHP_OS : " . PHP_OS; // PHP가 실행중인 OS의 이름 ?><br/>
+<? echo " TRUE : " . TRUE; // TRUE (참), 1을 표시 ?><br/>
+<? echo " FALSE : " . FALSE; // FALSE (거짓), 빈 값을 표시 ?><br/>
+<? echo " E_ERROR : " . E_ERROR; // 문법 오류가 아닌 복구가 불가능한 에러를 표시 ?><br/>
+<? echo " E_WARNING : " . E_WARNING; // PHP가 스크립트 실행에는 문제없으나 오류가 있음을 표시 ?><br/>
+<? echo " E_PARSE : " . E_PARSE; // 스크립트에서 문법적으로 잘못된 명령을 만난 경우 표시 ?><br/>
+<? echo " E_NOTICE : " . E_NOTICE; // 에러는 아니지만 알릴 사항이 있음을 표시 ?><br/>
+
---
+++ php/PhpstormProjects/php_test/execEcho.php
@@ -0,0 +1,6 @@
+<?php
+exec("ls -l", $output, $error);
+//exec('wkhtmltopdf http://somesite.com /home/user/file.pdf');
+echo "<pre>";
+print_r( $output );
+?>
